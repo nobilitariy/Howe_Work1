2befec499ddca02ec62e00928dfdc113d6f91e5b
nobilitariy <134000822+nobilitariy@users.noreply.github.com>
2023-06-16 12:24:47 +0000

    Add Task

---
+++ Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿{
+Console.WriteLine($"Enter the number corresponding to the day of the week(for example)
+
+
+int NumDaysWeak = Convert.ToInt32(Console.ReadLine());
+switch(NumDaysWeak)
+{
+    case 1:
+        Console.WriteLine("no");
+        break;
+    case 2:
+        Console.WriteLine("no");
+        break;
+    case 3:
+        Console.WriteLine("no");
+        break;
+    case 4:
+        Console.WriteLine("no");
+        break;
+    case 5:
+        Console.WriteLine("no");
+        break;
+    case 6:
+        Console.WriteLine("yes");
+        break;
+    case 7:
+        Console.WriteLine("yes");
+        break;
+    default:
+        Console.WriteLine("Enter the number corresponding to the day of the week");
+}
+
+
+

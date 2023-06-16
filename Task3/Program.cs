5ca65079837dc19184b7a10be4ca8fecc9950f79
nobilitariy <134000822+nobilitariy@users.noreply.github.com>
2023-06-16 16:23:45 +0000

    Final

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,30 +1,30 @@
 ﻿
-Console.WriteLine($"Enter the number corresponding to the day of the week(1-mon,2-tusd)");
+Console.WriteLine($"Enter the number corresponding to the day of the week(1-mon,2-tusd):");
 
 
 int NumDaysWeak = Convert.ToInt32(Console.ReadLine());
 switch(NumDaysWeak)
 {
     case 1:
-        Console.WriteLine("no");
+        Console.WriteLine("no, there are weekdaus");
         break;
     case 2:
-        Console.WriteLine("no");
+        Console.WriteLine("no, there are weekdaus");
         break;
     case 3:
-        Console.WriteLine("no");
+        Console.WriteLine("no, there are weekdaus");
         break;
     case 4:
-        Console.WriteLine("no");
+        Console.WriteLine("no, there are weekdaus");
         break;
     case 5:
-        Console.WriteLine("no");
+        Console.WriteLine("no, there are weekdaus");
         break;
     case 6:
-        Console.WriteLine("yes");
+        Console.WriteLine("yes, It's a day off");
         break;
     case 7:
-        Console.WriteLine("yes");
+        Console.WriteLine("yes, It's a day off");
         break;
     default:
         Console.WriteLine("Enter the number corresponding to the day of the week(1-mon,2-tusd)");

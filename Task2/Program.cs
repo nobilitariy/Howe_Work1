9441fdc4b78a3f654f55fb6167e16ddad352ff35
nobilitariy <134000822+nobilitariy@users.noreply.github.com>
2023-06-16 13:29:12 +0300

    Update Program.cs

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -14,7 +14,7 @@ int startRang = Convert.ToInt32(Console.ReadLine());
 int endRang = Convert.ToInt32(Console.ReadLine());
 int num3 = Convert.ToInt32(Console.ReadLine());
 int rendomNum = GetRand(startRang, endRang);
-int result2 = GetCalc(GetRand(startRang, endRang));
+int result2 = GetCalc(rendomNum);
 int result = GetCalc(num3);
 if(startRang > 99 && startRang < 1000 && endRang > 99 && endRang < 1000 && num3 > 99 && num3 < 1000)
 {
